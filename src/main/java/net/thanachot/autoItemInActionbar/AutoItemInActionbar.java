9567ff2de5f4f0f5cf13079fd901191d4f33f450
tantaihaha4487 <tantaiiottwo@gmail.com>
2025-11-22 04:58:17 +0700

    feat(bstats): Relocate bStats to internal package
    Integrates bStats by relocating it to net.thanachot.autoItemInActionbar.bstats and updates the plugin version in paper-plugin.yml. Removed bStats dependency from pom.xml for internal management.

--- src/main/java/net/thanachot/autoItemInActionbar/AutoItemInActionbar.java
+++ src/main/java/net/thanachot/autoItemInActionbar/AutoItemInActionbar.java
@@ -1,7 +1,7 @@
 package net.thanachot.autoItemInActionbar;
 
 import net.thanachot.autoItemInActionbar.listener.PlayerListener;
-import org.bstats.bukkit.Metrics;
+import net.thanachot.autoItemInActionbar.bstats.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AutoItemInActionbar extends JavaPlugin {
